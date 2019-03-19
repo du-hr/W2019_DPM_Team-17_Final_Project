b9aa976f17b10d89f39eaf352d6e4b6447c4317e
AymarMuhikira <aymar.muhikira@mail.mcgill.ca>
2019-03-19 17:16:58 -0400

    Added a correction to turning in navigation

--- NavigationUnitTestDemo/src/NavigationUnit/Navigation.java
+++ NavigationUnitTestDemo/src/NavigationUnit/Navigation.java
@@ -5,117 +5,172 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import static NavigationUnit.Main.*;
 
 public class Navigation {
-  private static Odometer odometer;
-  private static EV3LargeRegulatedMotor leftMotor;
-  private static EV3LargeRegulatedMotor rightMotor;
-  private static boolean navigating = false;
-  
+	private static Odometer odometer;
+	private static EV3LargeRegulatedMotor leftMotor;
+	private static EV3LargeRegulatedMotor rightMotor;
+	private static boolean navigating = false;
 
-  
-  public Navigation(Odometer odometer, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor){
-  }
-  
-  public static Navigation getNavigation(Odometer odometer, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor) {
 
-    Navigation navigator = new Navigation(odometer, leftMotor, rightMotor);
-    return navigator;
-  }
-  
-  public static void travelTo(double x, double y) {
-    
-    x = x * TILE_SIZE;
-    y = y * TILE_SIZE;
-    
-    
-    //reset motors
-        leftMotor.stop();
-        rightMotor.stop();
-        
-        leftMotor.setAcceleration(3000);
-        rightMotor.setAcceleration(3000);
 
-    
-    navigating = true;
-    
-    //calculate trajectory path and angle
-    double[] odoData = odometer.getXYT();
-    double X = odoData[0];
-    double Y = odoData[1];
-    double trajectoryX = x - X;
-    double trajectoryY = y - Y;
-    double trajectoryAngle = Math.toDegrees(Math.atan2(trajectoryX, trajectoryY));
-    
-    //rotate to correct angle
-    leftMotor.setSpeed(Main.ROTATE_SPEED);
-    rightMotor.setSpeed(Main.ROTATE_SPEED);
-    turnTo(trajectoryAngle);
-    
-    double trajectoryLine = Math.hypot(trajectoryX, trajectoryY);
-    
-    //move forward correct distance
-    leftMotor.setSpeed(FORWARD_SPEED);
-    rightMotor.setSpeed(FORWARD_SPEED);
-    leftMotor.rotate(convertDistanceForMotor(trajectoryLine),true);
-    rightMotor.rotate(convertDistanceForMotor(trajectoryLine),false);
-}
-  
-//to make sure the angle of each turn is the minimum angle possible
-  public static void turnTo(double heading) {
-      double[] odoData = odometer.getXYT();
-      double theta = odoData[2];
-      
-      double angle = heading-theta;
+	public Navigation(Odometer odometer, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor){
+	}
 
-      
-      if(angle < -180.0) {
-          angle = angle + 360;
-          turnRight(angle);
-      } 
-      else if (angle > 180.0) {
-          angle = angle - 360;
-          turnLeft(angle);
-          
-      } 
-      else if (angle < 0) {
-          turnLeft(angle);
-      }
-      else if(angle > 0) {
-          turnRight(angle);
-      }
-  }
-      
-      
-  private static void turnLeft (double angle) {
-      leftMotor.rotate(-convertAngleForMotor(Math.abs(angle)),true);
-      rightMotor.rotate(convertAngleForMotor(Math.abs(angle)),false);
-  }
-  
-  private static void turnRight (double angle) {
-      leftMotor.rotate(convertAngleForMotor(Math.abs(angle)),true);
-      rightMotor.rotate(-convertAngleForMotor(Math.abs(angle)),false);
-  }
-  
-  /* returns: whether or not the vehicle is currently navigating
-   */
-  public boolean isNavigating() {
-      return navigating;
-  }
-  
-  /**
-   * This method allows the conversion of a distance to the total rotation of each wheel need to
-   * cover that distance.
-   * 
-   * @param radius
-   * @param distance
-   * @return
-   */
-private static int convertDistanceForMotor(double distance){
-    return (int) (360*distance/(2*Math.PI*WHEEL_RADIUS));
-}
+	public static Navigation getNavigation(Odometer odometer, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor) {
 
-private static int convertAngleForMotor(double angle){
-    return convertDistanceForMotor(Math.PI*WHEEL_BASE*angle/360.0);
-}
+		Navigation navigator = new Navigation(odometer, leftMotor, rightMotor);
+		return navigator;
+	}
+
+	public static void travelTo(double x, double y) {
+
+		x = x * TILE_SIZE;
+		y = y * TILE_SIZE;
+
+
+		//reset motors
+		leftMotor.stop();
+		rightMotor.stop();
+
+		leftMotor.setAcceleration(3000);
+		rightMotor.setAcceleration(3000);
+
+
+		navigating = true;
+
+		//calculate trajectory path and angle
+		double[] odoData = odometer.getXYT();
+		double X = odoData[0];
+		double Y = odoData[1];
+		double trajectoryX = x - X;
+		double trajectoryY = y - Y;
+		double trajectoryAngle = Math.toDegrees(Math.atan2(trajectoryX, trajectoryY));
+
+		//rotate to correct angle
+		leftMotor.setSpeed(Main.ROTATE_SPEED);
+		rightMotor.setSpeed(Main.ROTATE_SPEED);
+		turnTo(trajectoryAngle);
+		double trajectoryLine = Math.hypot(trajectoryX, trajectoryY);
+		//move forward correct distance
+		leftMotor.setSpeed(FORWARD_SPEED);
+		rightMotor.setSpeed(FORWARD_SPEED);
+		leftMotor.rotate(convertDistanceForMotor(trajectoryLine),true);
+		rightMotor.rotate(convertDistanceForMotor(trajectoryLine),false);
+	}
+	private static double gyroFetch() {
+		gyro_sp.fetchSample(gyro_sample, 0);
+		angleCorrection();
+		return odometer.getXYT()[2];
+	}
+
+	private static void angleCorrection() {
+		gyro_sp.fetchSample(gyro_sample, 0);
+		if (gyro_sample[0] >= 0) {
+			odometer.setXYT(odometer.getXYT()[0], odometer.getXYT()[1],gyro_sample[0]);
+		}else {
+			odometer.setXYT(odometer.getXYT()[0], odometer.getXYT()[1], 360+gyro_sample[0]);
+		}
+	}
+	//to make sure the angle of each turn is the minimum angle possible
+	public static void turnTo(double heading) {
+		double[] odoData = odometer.getXYT();
+		double theta = odoData[2];
+		//theta = gyroFetch();
+		//Replace previous line of code with previous comment to test correction
+		//Also change turnRight and turnLeft with turnRight2 and turnLeft2
+		double angle = heading-theta;
+
+
+		if(angle < -180.0) {
+			angle = angle + 360;
+			turnRight(angle);
+		} 
+		else if (angle > 180.0) {
+			angle = angle - 360;
+			turnLeft(angle);
+
+		} 
+		else if (angle < 0) {
+			turnLeft(angle);
+		}
+		else if(angle > 0) {
+			turnRight(angle);
+		}
+	}
+
+
+	private static void turnLeft (double angle) {
+		leftMotor.rotate(-convertAngleForMotor(Math.abs(angle)),true);
+		rightMotor.rotate(convertAngleForMotor(Math.abs(angle)),false);
+	}
+
+	private static void turnRight (double angle) {
+		leftMotor.rotate(convertAngleForMotor(Math.abs(angle)),true);
+		rightMotor.rotate(-convertAngleForMotor(Math.abs(angle)),false);
+	}
+
+	private static void turnLeft2(double degree) {
+		if (degree <= 1) {
+			return;
+		}
+		int speed;
+		double minAngle = 0;
+		double angle = gyroFetch();
+		double angle1 = gyroFetch();
+		while((Math.abs(angle - angle1 - degree)>=1) && (Math.abs((angle1 - angle) - (360-degree))>=1)){
+			minAngle = Math.min((Math.abs(angle - angle1 - degree)), Math.abs((angle1 - angle) - (360-degree)));
+			speed = (int)(80 - 25/(minAngle+1));
+			leftMotor.setSpeed(speed);
+			rightMotor.setSpeed(speed);
+			leftMotor.backward();
+			rightMotor.forward();
+			angle1 = gyroFetch();
+		}
+		leftMotor.stop(true);
+		rightMotor.stop();
+	}
+
+	private static void turnRight2(double degree) {
+		if(degree <= 1) {
+			return;
+		}
+		double minAngle = 0;
+		int speed;
+		double angle = gyroFetch();
+		double angle1 = gyroFetch();
+		while((Math.abs(angle1 - angle - degree)>=1) && (Math.abs((angle - angle1) - (360-degree))>=1)){
+			minAngle = Math.min((Math.abs(angle1 - angle - degree)), Math.abs((angle - angle1) - (360-degree)));
+			speed = (int)(80 - 25/(minAngle+1));
+			leftMotor.setSpeed(speed);
+			rightMotor.setSpeed(speed);
+			leftMotor.forward();
+			rightMotor.backward();
+			angle1 = gyroFetch();
+		}
+		leftMotor.stop(true);
+		rightMotor.stop();
+	}
+
+	/* returns: whether or not the vehicle is currently navigating
+	 */
+	public boolean isNavigating() {
+		return navigating;
+	}
+
+	/**
+	 * This method allows the conversion of a distance to the total rotation of each wheel need to
+	 * cover that distance.
+	 * 
+	 * @param radius
+	 * @param distance
+	 * @return
+	 */
+	private static int convertDistanceForMotor(double distance){
+		return (int) (360*distance/(2*Math.PI*WHEEL_RADIUS));
+	}
+
+	private static int convertAngleForMotor(double angle){
+		return convertDistanceForMotor(Math.PI*WHEEL_BASE*angle/360.0);
+	}
 
 
 }
